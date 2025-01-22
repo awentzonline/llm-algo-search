eea41163b3341d7df836bca70f187506e4f728ab
Adam Wentz <adam@adamwentz.com>
2025-01-21 19:01:59 -0800

    wrap AseDBDataset in dataset that applies the atom vectorizer

--- llm_algo_search/algorithms/s2ef_rr/evaluator.py
+++ llm_algo_search/algorithms/s2ef_rr/evaluator.py
@@ -1,4 +1,3 @@
-from fairchem.core.datasets import AseDBDataset
 import numpy as np
 import torch
 from torch import nn
@@ -14,17 +13,13 @@ class S2EFRREvaluator:
         """
         all_model_dims = (128, 256, 512, 1024)
 
-        train_dataset = AseDBDataset(config=dict(src=cfg.train_dataset_path))
-        val_dataset = AseDBDataset(config=dict(src=cfg.val_dataset_path))
-
         all_metrics = []
         for model_dims in all_model_dims:
             print('training model size =', model_dims)
             atom_rr = atom_rr_cls(model_dims)
             model = EnergyModel(model_dims, 1)
-
-            metrics = train_model(model, train_dataset, atom_rr, cfg)
-            eval_metrics = eval_model(model, val_dataset, atom_rr, cfg)
+            metrics = train_model(model, atom_rr, cfg)
+            eval_metrics = eval_model(model, atom_rr, cfg)
             all_metrics.append(eval_metrics)
 
         return {
--- llm_algo_search/algorithms/s2ef_rr/model.py
+++ llm_algo_search/algorithms/s2ef_rr/model.py
@@ -1,45 +1,65 @@
+from fairchem.core.datasets import AseDBDataset
 import torch
 from torch import nn
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
 
-def train_model(model, dataset, atom_rr, cfg):
+class AtomsReducedRepresentationDataset(Dataset):
+    def __init__(self, base_dataset, atom_rr):
+        self.base_dataset = base_dataset
+        self.atom_rr = atom_rr
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def __getitem__(self, idx):
+        item = self.base_dataset[idx]
+        reduced = self.atom_rr.prepare_inputs(
+            item.pos.float(),
+            item.atomic_numbers
+        )
+        return reduced, torch.FloatTensor([item.energy])
+
+
+def train_model(model, atom_rr, cfg):
     model.train()
     f_loss = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
+
+    dataset = AseDBDataset(config=dict(src=cfg.train_dataset_path, a2g_args=dict(r_energy=True)))
+    vec_dataset = AtomsReducedRepresentationDataset(dataset, atom_rr)
+    dataloader = DataLoader(vec_dataset, batch_size=cfg.batch_size)
+
     for epoch_i in range(cfg.num_epochs):
-        for i in tqdm(range(len(dataset))):
-            atoms = dataset.get_atoms(i)
-            inputs = atom_rr.prepare_inputs(
-                torch.from_numpy(atoms.get_atomic_numbers()),
-                torch.from_numpy(atoms.get_positions()).float()
-            )
+        for inputs, target_energy in tqdm(dataloader):
             pred_energy = model(inputs)
-            targets = torch.FloatTensor([atoms.get_total_energy()])
-            loss = f_loss(pred_energy, targets)
+            loss = f_loss(pred_energy, target_energy)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
     return {
         'loss': loss.item()
     }
 
 
 @torch.inference_mode
-def eval_model(model, dataset, atom_rr, cfg):
+def eval_model(model, atom_rr, cfg):
     model.eval()
     f_loss = nn.MSELoss()
+
+    dataset = AseDBDataset(config=dict(src=cfg.val_dataset_path, a2g_args=dict(r_energy=True)))
+    vec_dataset = AtomsReducedRepresentationDataset(dataset, atom_rr)
+    dataloader = DataLoader(vec_dataset, batch_size=cfg.batch_size)
+
     losses = []
-    for i in tqdm(range(len(dataset))):
-        atoms = dataset.get_atoms(i)
-        inputs = atom_rr.prepare_inputs(
-            torch.from_numpy(atoms.get_atomic_numbers()),
-            torch.from_numpy(atoms.get_positions()).float()
-        )
-        pred_energy = model(inputs)
-        targets = torch.FloatTensor([atoms.get_total_energy()])
-        loss = f_loss(pred_energy, targets)
-        losses.append(loss)
+    for epoch_i in range(cfg.num_epochs):
+        for inputs, target_energy in tqdm(dataloader):
+            pred_energy = model(inputs)
+            loss = f_loss(pred_energy, target_energy)
+            losses.append(loss)
+
     return {
         'mean_loss': torch.mean(torch.FloatTensor(losses)).item()
     }
