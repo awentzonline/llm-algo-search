ce9f370efbf039654ce8ad8bf37066822e748489
Adam Wentz <adam@adamwentz.com>
2025-01-24 18:50:53 -0800

    Add labels to distill api and label-only seed

--- llm_algo_search/algorithms/distill_clm/api.py
+++ llm_algo_search/algorithms/distill_clm/api.py
@@ -1,9 +1,10 @@
 from torch import nn
 
 class API(nn.Module):
-    def forward(self, student_logits, target_logits):
+    def forward(self, student_logits, target_logits, labels):
         """
         student_output.shape (batch, sequence, model)
         target_output.shape (batch, sequence, model)
+        labels.shape (batch, sequence)
         """
         pass
